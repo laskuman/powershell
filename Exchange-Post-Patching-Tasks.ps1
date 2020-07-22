65864b59569b27a8976df07a15ff6b5e6cfa39bf
laskuman <nicdsperez@gmail.com>
2020-07-23 06:40:07 +0800

    added exchange patching scripts

---
+++ Exchange-Post-Patching-Tasks.ps1
@@ -0,0 +1,40 @@
+﻿[CmdletBinding()]
+param (
+        [Parameter( Mandatory=$true)]
+        [string]$emailRecipient
+    )
+
+$currentServer = $env:COMPUTERNAME
+Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
+
+Write-Host "Activating Server"
+Set-ServerComponentState $currentServer –Component ServerWideOffline –State Active –Requester Maintenance
+
+Write-Host "Resuming cluster node"
+Resume-ClusterNode –Name $currentServer
+
+Write-Host "Reverting Database Copy Auto Activation Policy to previous setting"
+$DBActivationPolicy = Get-Content C:\temp\DBactivationpolicy.txt
+    if ($DBActivationPolicy)
+    {
+    Set-MailboxServer $currentServer –DatabaseCopyAutoActivationPolicy $DBActivationPolicy
+    }
+        else
+        {
+        Set-MailboxServer $currentServer –DatabaseCopyAutoActivationPolicy Unrestricted
+        }
+
+Write-Host "Enabling DB activation on server"
+Set-MailboxServer $currentServer –DatabaseCopyActivationDisabledAndMoveNow $false
+
+Write-Host "Activating HubTransport"
+Set-ServerComponentState $currentServer –Component HubTransport –State Active –Requester Maintenance
+
+
+$timestamp = Get-Date -Format "MMddyyyy-HHmm"
+$reportName = "C:\temp\ExchangePostPatchingReport_$timestamp.html"
+#$emailRecipient = "administrator@test.lab"
+#$emailSender = "administrator@test.lab"
+$emailSubject = ""
+
+Invoke-Expression -Command ".\Test-ExchangeServerHealth.ps1 -ReportMode -SendEmail -EmailServer $currentServer -EmailRecipient $emailRecipient"
---
+++ Exchange-Pre-Patching-Tasks.ps1
@@ -0,0 +1,27 @@
+﻿$currentServer = $env:COMPUTERNAME
+Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
+
+#Set the HubTransport component to “Draining”, and redirect any messages currently in the queue to another server.
+Set-ServerComponentState $currentServer –Component HubTransport –State Draining –Requester Maintenance
+Redirect-Message -Server $currentServer -Target EX2016SRV2.exchangeserverpro.net
+
+#Suspend the node from the cluster
+Suspend-ClusterNode –Name $currentServer
+
+#Disable database copy auto-activation. This command will also move any active database copies to other DAG members, assuming there are other healthy DAG members available. This is not instantaneous, it can take several minutes for the moves to occur. 
+Set-MailboxServer $currentServer –DatabaseCopyActivationDisabledAndMoveNow $true
+
+#Get current DB activation policy
+$currentDBActivationPolicy = Get-MailboxServer $currentServer | Select -ExpandProperty DatabaseCopyAutoActivationPolicy
+Set-Content -Path C:\temp\DBactivationpolicy.txt -Value $currentDBActivationPolicy
+If ($currentDBActivationPolicy -ne "Blocked")
+    {
+    Set-MailboxServer $currentServer –DatabaseCopyAutoActivationPolicy Blocked
+    }
+
+Get-MailboxDatabaseCopyStatus -Server $currentServer | Where {$_.Status -eq "Mounted"}
+
+Set-ServerComponentState $currentServer –Component ServerWideOffline –State InActive –Requester Maintenance
+
+
+#Set-MailboxServer $currentServer –DatabaseCopyAutoActivationPolicy Unrestricted
